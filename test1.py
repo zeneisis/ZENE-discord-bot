166d862a4e127c647acfd4cd9d5e602221fcf3f1
zeneisis <95860050+zeneisis@users.noreply.github.com>
2025-01-15 16:21:07 +0900

    Delete test1.py

--- test1.py
+++
@@ -1,7 +0,0 @@
-import ebooklib
-from ebooklib import epub
-
-book = epub.read_epub('C:\Users\takah\Downloads\Konosuba.epub')
-
-for image in book.get_items_of_type(ebooklib.ITEM_IMAGE):
-    print(image)
