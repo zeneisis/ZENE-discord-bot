7a8ff23882d88d29a1ee0909b717ff4ac50c5953
zeneisis <95860050+zeneisis@users.noreply.github.com>
2025-01-15 16:25:07 +0900

    Delete zene/test.py

--- zene/test.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image, ImageDraw, ImageFilter
-
-haha = Image.open('zene/data/image/haha.jpg')
-blurImage = haha.filter(ImageFilter.BoxBlur(15))
-blurImage.save('zene/data/image/simBlurImage.jpg')
